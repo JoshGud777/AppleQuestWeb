4652153819e76cb0b8816c6787cd930292ad9dac
JoshuaGud777 <joshgud777@gmail.com>
2015-02-10 22:08:02 -0600

    pep8

--- wwwApp/donoteverrun.py
+++ wwwApp/donoteverrun.py
@@ -1,10 +1,20 @@
-import os, binascii
+import binascii
+import os
+
 def main():
     print("Content-type: text/plain\n")
     x = 0
-    while x < 1000:
+    while x < 50:
         y = binascii.hexlify(os.urandom(16)).decode('utf8')
-        if y[0] == 'd' and y[1] == 'b' and y[-2]== 'd' and y[-1] == 'b':
-            print(str(x+1) + ': ' + y)
+        if y[0] == 'd' and y[1] == 'b' and y[-2] == 'd' and y[-1] == 'b':
+
+            if x < 9:
+                z = ' :'
+            else:
+                z = ':'
+
+            print(str(x+1) + z + y)
             x += 1
-main()
+
+if __name__ == '__main__':
+    main()
