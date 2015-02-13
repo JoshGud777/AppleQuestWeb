30670b27aeeb2ac4499123e1e0440975928de436
JoshuaGud777 <joshgud777@gmail.com>
2015-02-12 18:54:45 -0600

    logon VAR fix

--- wwwApp/login.py
+++ wwwApp/login.py
@@ -4,7 +4,7 @@ def main():
     lib.print_header()
     form = lib.get_cgi_data()
     if form.getvalue('key') == 'send_login':
-        lib.print_me(lib.DIR_REDIRECT + 'to_game.html')
+        lib.print_me(lib.REDIRECT_DIR + 'to_game.html')
     else:
         lib.print_me(lib.HTML_DIR + 'login0.html')
         lib.print_me(lib.HTML_DIR + 'login1.html')
