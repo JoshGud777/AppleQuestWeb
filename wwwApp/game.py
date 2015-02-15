8ec473ee82a7ec211ea9b67bc7dfc7eb56c45cdb
JoshuaGud777 <joshgud777@gmail.com>
2015-02-15 14:43:01 -0600

    last command saves

--- wwwApp/game.py
+++ wwwApp/game.py
@@ -2,43 +2,48 @@ import library as lib
 import cgitb
 cgitb.enable()
 
+def html(value='', print_data=''):
+    if print_data != '':
+        print_data = '<p>' + print_data + '</p>'
+    else:
+        print_data = '<!--Print data goes here, if there is any-->'
+        
+    print('''<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>AppleQuest - Game</title>
+</head>
+<body>
+''' + print_data + '''
+<form action="game.py">
+Command: 
+<input type="text" name="command" value="''' + value + '''">
+<br><br>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>''')
+
 def cookie_read():
-    
     cookie = lib.get_cookies()
     if cookie == None:
-        print('No Cookies')
+        print('<!-- No Cookies -->')
     else:
-        print(cookie)
+        print('<!-- ', end="")
+        print(cookie + "     |     ", end="")
+        print('-->')
         
-def main():
+def main(x):
     lib.print_header()
     cookie_read()
+    form = lib.get_cgi_data()
+    x = form.getfirst("command")
+    if x == None:
+        x = ''
+    elif type(x) != str:
+        x = str(x)
+    html(x)
     
 if __name__ == '__main__':
     main()
-
-
-
-
-'''
-    c.execute("SELECT username FROM logon WHERE username = ?", [username])
-    dbdata = c.fetchone()
-    
-    if not dbdata == None:
-        dbusername = dbdata[0]
-        if dbusername == username:
-            return False
-'''
-
-
-    
-
-
-def cookie_wright(sessionid, exp, username):
-    headder()
-    cookie = http.cookies.BaseCookie()
-    cookie['id'] = sessionid
-    cookie['exp'] = exp
-    cookie['username'] = username
-    print(cookie)
-    print()
