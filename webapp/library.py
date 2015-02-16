456593ce20ec7a7b203ab5aa3d351bbb39ca9aac
JoshuaGud777 <joshgud777@gmail.com>
2015-02-16 13:26:29 -0600

    pylint, closes #7

--- webapp/library.py
+++ webapp/library.py
@@ -1,6 +1,7 @@
+'''This is the library files for all used code
+that needs to be uused multi times'''
 import binascii
 import cgi
-import datetime
 import hashlib
 import http.cookies
 import os
@@ -14,28 +15,38 @@ COOKIE_MAX_AGE = 300
 COOKIE_DOMAIN = 'applequest.fallenofftheedge.com'
 COOKIE_PATH = '/'
 
+conn = None
+c = None
 
-def open_conn(db):
+
+def open_conn(database):
+    '''Open SQL Connection to a given sqlite databsase'''
     global conn
     global c
-    conn = sqlite3.connect(db)
+    conn = sqlite3.connect(database)
     c = conn.cursor()
 
 
 def save_conn():
+    '''Savesthe conn'''
     conn.commit()
 
 
 def save_close_conn():
+    '''Saves and closes the conn'''
     conn.commit()
     conn.close()
 
 
 def close_conn():
+    '''Closes the database conn'''
     conn.close()
 
 
 def add_user(username, pword, email=None):
+    '''For a givven username and pasword and maybe an e-mail. Adds the user to
+    the database. If the user is allready there then it returns false. if it
+    added the database it sends True'''
     display = username[:]
     username = username.lower()
 
@@ -46,13 +57,20 @@ def add_user(username, pword, email=None):
     hashed_salted_password = hashlib.sha512(utf8pword_salt)
     enchexpass = hashed_salted_password.hexdigest()
     try:
-        c.execute("INSERT INTO logon VALUES (?, ?, ?, ?, ?)", (username, display, enchexpass, salt, email))
+        c.execute("INSERT INTO logon VALUES (?, ?, ?, ?, ?)", (username,
+                                                               display,
+                                                               enchexpass,
+                                                               salt, email))
     except:
         return False
     return True
 
 
 def issue_session_id(username, pword):
+    '''issues a session id for a given username, checks the user and pass
+    agenst the db then sends back a sessionif, epx, and theusername it is sent
+    agenst | noauth means username and password is wrrong | sqlerror means the
+    server is haveing issues'''
     username = username.lower()
     authuser = check_user(username, pword)
     if authuser is True:
@@ -60,17 +78,20 @@ def issue_session_id(username, pword):
         sqlretry = 0
         sqlgood = False
         while sqlgood is False:
-            c.execute("SELECT * FROM logon WHERE username = ?", [username])
-            dbdata = c.fetchone()
-            db_username = dbdata[0]
-            db_display = dbdata[1]
+            # c.execute("SELECT * FROM logon WHERE username = ?", [username])
+            # dbdata = c.fetchone()
+            # db_username = dbdata[0]
+            # db_display = dbdata[1]
 
-            exp = int(time.time()) + 300  # seconds till this is expired | 300 = 5 min | 1 = 1 sec
+            exp = int(time.time()) + 300
+            # seconds till this is expired | 300 = 5 min | 1 = 1 sec
             sessionid = binascii.hexlify(os.urandom(16)).decode("utf-8")
 
             try:
-                c.execute("DELETE FROM sessions WHERE username = ?", [username])
-                c.execute("INSERT INTO sessions VALUES (?, ?, ?)", [sessionid, exp, username])
+                c.execute("DELETE FROM sessions WHERE username = ?",
+                          [username])
+                c.execute("INSERT INTO sessions VALUES (?, ?, ?)",
+                          [sessionid, exp, username])
                 sqlgood = True
             except:
                 sqlretry += 1
@@ -83,8 +104,12 @@ def issue_session_id(username, pword):
 
 
 def renew_session_id(old_id, username):
+    '''givven the old session id and username it checks that the session is
+    is still good then send a newone if OK, else it sends out a "sqlerror" in
+    the case the server is erroring and a "expired" if the session is old'''
     username = username.lower()
-    c.execute("SELECT * FROM sessions WHERE username =  ? AND id = ?", [username, old_id])
+    c.execute("SELECT * FROM sessions WHERE username =  ? AND id = ?",
+              [username, old_id])
     dbdata = c.fetchone()
     if dbdata is None:
         return False
@@ -93,39 +118,49 @@ def renew_session_id(old_id, username):
     print(int(time.time()))
     print(db_exp)
     if int(time.time()) > db_exp:
-        return ('expired', 'expired')
+        return 'expired'
     elif int(time.time()) <= db_exp:
         sqlgood = False
         sqlretry = 0
         while sqlgood is False:
-            exp = int(time.time()) + 300  # seconds till this is expired | 300 = 5 min | 1 = 1 sec
+            exp = int(time.time()) + 300
+            # seconds till this is expired | 300 = 5 min | 1 = 1 sec
             sessionid = binascii.hexlify(os.urandom(512)).decode("utf-8")
             try:
-                c.execute("DELETE FROM sessions WHERE username = ?", [username])
-                c.execute("INSERT INTO sessions VALUES (?, ?, ?)", [sessionid, exp, username])
+                c.execute("DELETE FROM sessions WHERE username = ?",
+                          [username])
+                c.execute("INSERT INTO sessions VALUES (?, ?, ?)",
+                          [sessionid, exp, username])
                 sqlgood = True
             except:
                 sqlretry += 1
                 if sqlretry == 10:
-                    return ('sqlerror')
+                    return 'sqlerror'
 
         return (sessionid, exp, username)
 
 
 def delete_session(sessionid, username):
+    '''deletes a session from the database in the case the client wants to
+    "logoff"'''
     username = username.lower()
-    c.execute("SELECT * FROM sessions WHERE username = ? OR id = ?", [username, sessionid])
+    c.execute("SELECT * FROM sessions WHERE username = ? OR id = ?",
+              [username, sessionid])
     dbdata = c.fetchone()
     if dbdata is None:
         return True
 
-    c.execute("DELETE FROM sessions WHERE username = ? OR id = ?", [username, sessionid])
+    c.execute("DELETE FROM sessions WHERE username = ? OR id = ?",
+              [username, sessionid])
     return True
 
 
 def check_user(username, pword):
+    '''checks the username and password agenst the data base loaded with the
+    open_conn(), returns True is they are correct'''
     username = username.lower()
-    c.execute("SELECT username, password, salt FROM logon WHERE username = ?", [username])
+    c.execute("SELECT username, password, salt FROM logon WHERE username = ?",
+              [username])
     dbdata = c.fetchone()
 
     if dbdata is None:
@@ -147,6 +182,8 @@ def check_user(username, pword):
 
 
 def slow_equal(a, b):
+    '''Instead of comparing the string with == it checkes each part on at a
+    time, this makes it slower and therefor harder to crack.'''
     length = 0
     equal = 0
 
@@ -169,6 +206,8 @@ def slow_equal(a, b):
 
 
 def cookie_wright(sessionid, exp, username):
+    '''give the imput data it returns a session cookie ment to be placed in the
+    print_header function to send to the client'''
     cookie = http.cookies.BaseCookie()
     cookie['id'] = sessionid
     cookie['exp'] = exp
@@ -181,6 +220,8 @@ def cookie_wright(sessionid, exp, username):
 
 
 def get_cookies():
+    '''returns a cookie opject of the request header sent to the server from
+    the client'''
     cookie = http.cookies.BaseCookie()
     if 'HTTP_COOKIE' in os.environ:
         cookie.load(os.environ['HTTP_COOKIE'])
@@ -188,15 +229,9 @@ def get_cookies():
     return None
 
 
-def print_me(filename):
-    f = open(filename, 'r')
-
-    for line in f:
-        print(line, end=' ')
-    f.close
-
-
 def print_header(cookie=''):
+    '''Prints the standard HTTP header needed by CGI along with any cookie data
+    sent to the function - cookie must be a cookie object'''
     print('Content-type: text/html')
     print('Status: 200 OK')
     print(cookie)
@@ -205,5 +240,6 @@ def print_header(cookie=''):
 
 
 def get_cgi_data():
+    '''gets the cgi data from the last form the client summited'''
     cgidata = cgi.FieldStorage()
     return cgidata
