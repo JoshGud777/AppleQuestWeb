e38dd4bf5413c9bf7ee010342284557c780dc5a7
JoshuaGud777 <joshgud777@gmail.com>
2015-02-24 23:41:43 -0600

    Cleaner Fix/Update

--- cleanner.bat
+++ cleanner.bat
@@ -9,9 +9,10 @@ echo ### --- RD /S /Q webapp_tests\__pycache__
 
 del *.coverage*
 del *.log
+del webapp\db17b1a5c2b2f6d370af2c59c885d5db\*journal
+
 RD /S /Q coverage
 RD /S /Q python_tests_xml
 RD /S /Q __pycache__
 RD /S /Q webapp\__pycache__
 RD /S /Q webapp_tests\__pycache__
-RD /S /Q webapp\db17b1a5c2b2f6d370af2c59c885d5db\*.db-journal
