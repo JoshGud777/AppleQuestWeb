6fcf61a84320095ea7f731351cc5e94cff77134c
JoshuaGud777 <joshgud777@gmail.com>
2015-02-25 19:10:34 -0600

    Login.py now uses html!

--- webapp/login.py
+++ webapp/login.py
@@ -6,24 +6,15 @@ import cgitb
 cgitb.enable()
 
 
-def html():
+def html(error=''):
     '''Prints all the HTML for the page.'''
-    print('''<!DOCTYPE html>
-<html>
-<body>
-<form action="/login.py" method="POST">
-First name:<br>
-<input type="text" name="username">
-<br>
-Last name:<br>
-<input type="password" name="password">
-<br><br>
-<input type="hidden" name="key" value="send_login">
-<input type="submit" value="Submit">
-</form>
-</body>
-</html>''')
-
+    html = lib.get_html(lib.HTML_DIR + 'login.html')
+    if error != '':
+        error = str(error)
+        html = html.replace('$$ERROR', error)
+    else:
+        html = html.replace('$$ERROR$$', '')
+    print(html)
 
 def main():
     '''main - takes imput from user and issues session id for that user'''
@@ -33,12 +24,27 @@ def main():
         username = form['username'].value
         password = form['password'].value
 
-        sessionid, exp, username = lib.issue_session_id(username, password)
-
-        cookie = lib.cookie_wright(sessionid, exp, username)
-
-        lib.print_header(cookie=cookie)
-        lib.print_me(lib.REDIRECT_DIR + 'to_game.html')
+        session_info = lib.issue_session_id(username, password)
+        if session_info[0] == 'noauth':
+            lib.print_header()
+            error = '!!! Invalid Username OR Password !!!'
+            html(error)
+            
+        elif session_info[0] == 'sqlerror':
+            lib.print_header()
+            error = 'Internal SQL error. </br> Please e-mail servgud777@gmail.com!'
+            html(error)
+            
+        elif username == session_info[2]:
+            sessionid, exp, username = session_info
+            cookie = lib.cookie_wright(sessionid, exp, username)
+            lib.print_header(cookie=cookie)
+            lib.print_me(lib.REDIRECT_DIR + 'to_game.html')
+        else:
+            lib.print_header()
+            error = 'Internal SERVER error. </br> Please e-mail servgud777@gmail.com!'
+            html(error)
+            
     else:
         lib.print_header()
         html()
