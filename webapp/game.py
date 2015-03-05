49a7fde8879a2414bef13930cbf89e81d8995acf
JoshuaGud777 <joshgud777@gmail.com>
2015-03-05 02:26:56 -0600

    Renewal and commands

--- webapp/game.py
+++ webapp/game.py
@@ -4,13 +4,24 @@ import cgitb
 cgitb.enable()
 
 
-def html_print(value='', print_data=''):
+def html_print(value='', print_data='', newcookie=''):
     '''Prints the HTML to the client with varibals
        $$   printdata  $$
        $$   command    $$
-       $$ printcommand $$'''
+       $$ newcookie    $$
+       $$ oldcookie    $$'''
     html = lib.get_html(lib.HTML_DIR + 'game.html')
 
+    if True:
+        html = html.replace('$$oldcookie$$$', cookie_read())
+    else:
+        html = html.replace('$$oldcookie$$$', '')
+
+    if newcookie != '':
+        html = html.replace('$$newcookie$$$', newcookie)
+    else:
+        html = html.replace('$$newcookie$$$', '')
+        
     if print_data != '':
         html = html.replace('$$printdata$$', print_data)
     else:
@@ -27,7 +38,6 @@ def html_print(value='', print_data=''):
         print('This is not the case you are looking for!')
         print('P.S. The world is about to end!!!')
 
-    print(cookie_read())
     print(html)
 
 
@@ -36,32 +46,44 @@ def cookie_read():
     to the client'''
     cookie = lib.get_cookies()
     if cookie is None:
-        return '<!-- No Cookies -->'
+        return 'No saved Cookies'
     else:
-        returndata = '<!--\n'
-        returndata += str(cookie)
-        returndata += '-->'
-
-    return returndata
+        return str(cookie)
 
 
 def main():
     '''main'''
-    lib.print_header()
+    print_data = ''
+    newcookie = 'No New Cookie'
     cookie_read()
     form = lib.get_cgi_data()
     command = form.getfirst("command")
-    print_data = form.getfirst("print")
+    renew = form.getfirst("newid")
     if command is None:
         command = ''
     elif type(command) != str:
         command = str(command)
 
-    if print_data is None:
-        print_data = ''
-    elif type(print_data) != str:
-        print_data = str(print_data)
-    html_print(command, print_data)
+    cookie = ''
+    if renew == 'true':
+        cookies = lib.get_cookies()
+        sessioninfo = renew_session_id(cookies['id'],cookies['username'])
+        if type(sessioninfo) == str:
+            print_data += 'Could not renew\n'
+        else:
+            newcookie = lib.cookie_wright(sessioninfo)
+
+    if command == '301':
+        print_data += '103\n'
+    elif command == '302':
+        print_data += '203\n'
+    elif command == '303':
+        print_data += '303\n'
+    else:
+        print_data += '003\n'
+        
+    lib.print_header(cookie)
+    html_print(command, print_data, newcookie)
 
 if __name__ == '__main__':
     main()
